bbe92e38c2e36a234df31bfceca5d3691987f56c
Ebon97 <ebonwong@gmail.com>
2017-11-14 18:00:23 +0800

    Adding DefaultSelectedShipTesting

---
+++ DefaultSelectedShipTesting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+using NUnit.Framework;
+namespace MyGame
+{
+	[TestFixture ()]
+	public class UnitTesting
+	{
+		[Test ()]
+		public void DefaultSelectedShip ()
+		{
+			ShipName _selectedShip = ShipName.Tug;
+			ShipName selected = default (ShipName);
+
+			if (selected != ShipName.None) {
+				_selectedShip = selected;
+			}
+
+			Assert.AreEqual (selected, _selectedShip);
+
+		}
+
+	}
+}
