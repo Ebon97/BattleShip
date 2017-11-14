70e1ddee28de43594437507aa88147c08b779f4c
Stella97 <31676244+Stella97@users.noreply.github.com>
2017-11-14 17:14:05 +0800

    LevelDifficultyTest
    Testing on level of difficulty

---
+++ src/Model/LevelDifficultyTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MyGame
+{
+	[TestFixture()]
+	public class LevelDifficultyTest
+	{
+		[Test ()]
+		public void EasyLevelTest ()
+		{
+			MenuController.PerformSetupMenuAction (0);
+			Assert.IsTrue (GameController._aiSetting == AIOption.Easy);
+		}
+
+		[Test ()]
+		public void MediumLevelTest ()
+		{
+			MenuController.PerformSetupMenuAction (1);
+			Assert.IsTrue (GameController._aiSetting == AIOption.Medium);
+		}
+	}
+}
