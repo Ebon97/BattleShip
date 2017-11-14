bc44633bbe493bb415cc1779a58689c363e0817e
hamxaamir <hamzaameer22@gmail.com>
2017-11-14 22:23:37 +0800

    * MyGame.exe: test hamza
    * MyGame.csproj:
    * MyGame.userprefs:
    * TestState.cs:
    * GameState Test.cs:
    * LevelDiffucltyTest.cs:
    * MyGame.exe:
    * LevelDifficultyTest.cs:
    * MyGame.csproj.FileListAbsolute.txt:
    * MyGame.csprojResolveAssemblyReference.cache:

---
+++ src/Model/GameState Test.cs
@@ -0,0 +1,7 @@
+﻿using System;
+namespace MyGame
+{
+	public struct GameState_Test
+	{
+	}
+}
---
+++ src/Model/TestState.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+namespace MyGame
+{
+	[TestFixture()]
+	public class TestState
+	{
+		[Test ()]
+		public void TestStateAdd ()
+		{
+			GameController.AddNewState (GameState.AlteringSettings);
+			Assert.AreEqual (GameController.CurrentState, GameState.AlteringSettings);
+
+			GameController.AddNewState (GameState.EndingGame);
+			Assert.AreEqual (GameController.CurrentState, GameState.EndingGame);
+
+			GameController.EndCurrentState ();
+			Assert.AreEqual (GameController.CurrentState, GameState.AlteringSettings);
+		}
+	}
+}
